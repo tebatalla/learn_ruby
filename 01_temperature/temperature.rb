cb5cee68b1380a6ecc63556f39a7935c9da36914
Tomas <tebatalla@gmail.com>
2014-09-20 11:54:48 -0500

    Added settings to remove deprecation warnings to config.rb. Tests pass for hello.rb and temperature.rb

---
+++ 01_temperature/temperature.rb
@@ -0,0 +1,7 @@
+def ftoc(degree)
+	result = (degree - 32) * 5 / 9
+end
+
+def ctof(degree)
+	result = (degree * 9.0 / 5.0) + 32.0
+end
--- rspec_config.rb
+++ rspec_config.rb
@@ -2,3 +2,13 @@ RSpec.configure do |c|
   c.fail_fast = true
   c.color = true
 end
+
+RSpec.configure do |config|
+  # ...
+  config.mock_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
